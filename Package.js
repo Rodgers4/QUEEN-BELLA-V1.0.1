17bc2a96bdb43db804c9267e98ff27ac0f7e530f
Rodgers4 <rogersonyango568@gmail.com>
2025-07-13 19:10:42 +0300

    Create Package.js
    Package

---
+++ Package.js
@@ -0,0 +1,16 @@
+{
+  "name": "queen-bella",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js && node server.js"
+  },
+  "dependencies": {
+    "@adiwajshing/baileys": "^6.6.0",
+    "@google/generative-ai": "^0.5.0",
+    "@hapi/boom": "^10.0.1",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "openai": "^4.29.1"
+  }
+}
